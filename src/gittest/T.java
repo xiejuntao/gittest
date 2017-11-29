3a68d40c9557a20dfef103c32cd9abe7cc052367
xiejuntao <xiejuntao@lizhi.fm>
2017-11-29 23:09:27 +0800

    love

--- src/gittest/T.java
+++ src/gittest/T.java
@@ -4,7 +4,8 @@ public class T {
 
 	public static void main(String[] args) {
 		System.out.println(3);
-		System.out.println("记否，记否，昨日又相见，今日是否应相识。");
+		System.out.println("记否，记否，昨日又相见，今日是否应相识！");
+		//爱，怎么说出口。
 	}
 
 }
