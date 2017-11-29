0a011fcaf691fe1e7e01e1d3c792558d13e209d6
xiejuntao <xiejuntao@lizhi.fm>
2017-11-29 23:06:46 +0800

    t

--- src/gittest/T.java
+++ src/gittest/T.java
@@ -3,8 +3,8 @@ package gittest;
 public class T {
 
 	public static void main(String[] args) {
-	      System.out.println(2);
-
+		System.out.println(3);
+		System.out.println("记否，记否，昨日又相见，今日是否应相识。");
 	}
 
 }
