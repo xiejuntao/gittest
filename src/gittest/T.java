1cd46df399507ac0d60fc26943aaa54d65cc50d2
xiejuntao <xiejuntao@lizhi.fm>
2017-11-29 23:20:16 +0800

    la la

--- src/gittest/T.java
+++ src/gittest/T.java
@@ -5,6 +5,6 @@ public class T {
 	public static void main(String[] args) {
 		System.out.println(3);
 		System.out.println("记否，记否，昨日又相见，今日是否应相识！");
-		//爱，怎么说出口。勇敢爱。
+		// 爱，怎么说出口。勇敢爱。爱没那么简单。
 	}
 }
